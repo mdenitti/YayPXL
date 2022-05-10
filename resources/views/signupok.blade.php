362572128d3d4580359df712b52cdd8245db1c46
mdenitti <massimodn77@gmail.com>
2022-05-10 14:03:10 +0200

    update

--- resources/views/signupok.blade.php
+++ resources/views/signupok.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.theme')
 
 @section('content')
-<h1>Thank you for your confidence</h1>
+<h1>Thank you for your confidence {{$validation['name']}}</h1>
 
-{{$course}}
+You have enrolled in our class:
+<h4>{{$course}}</h4>
 
-{{$validation['name']}}
-
-
+Your details are:
+<div>
+{{$validation['name']}} <hr>
+{{$validation['email']}} <hr>
+{{$validation['phone']}} <hr>
+{{$validation['bdate']}} <hr>
+</div>
 
 @endsection
